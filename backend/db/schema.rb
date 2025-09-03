e002a687ef67bd776ece0772e12f30b7c7965297
rafaella-martino <rafaella.martino@cedarcode.com>
2025-09-02 21:21:46 -0300

    feat: add modality and capacity to tutoring

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_02_234613) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_03_001926) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -31,6 +31,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_02_234613) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "duration_mins", default: 60, null: false
+    t.string "modality", null: false
+    t.integer "capacity", null: false
   end
 
   create_table "users", force: :cascade do |t|
