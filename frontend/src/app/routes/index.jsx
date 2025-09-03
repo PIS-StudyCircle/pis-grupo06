7ad53aaf60eae9beb42bc8bd15723fd7046e48e0
Camila <cami.cam2906@gmail.com>
2025-09-03 00:49:02 -0300

    ruta a coursePage (temporal)

--- frontend/src/app/routes/index.jsx
+++ frontend/src/app/routes/index.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import { FacultiesPage } from "@/features/faculties";
+import CoursePage from "@/features/courses/pages/CoursePage";
 
 export function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<FacultiesPage />} />
+      <Route path="/courses" element={<CoursePage />} />
     </Routes>
   );
 }
