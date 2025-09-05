171b670d6e7a6cf71b05921f0c1bf7385161c956
rafaella-martino <rafaella.martino@cedarcode.com>
2025-09-04 22:36:07 -0300

    refactor: fix indentation and use .env to reference api url

--- frontend/src/features/courses/components/CourseCard.jsx
+++ frontend/src/features/courses/components/CourseCard.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import CourseButton from "./CourseButton";
 
 export default function CourseCard({ course }) {
-    return (
-        <div className="h-20 bg-white rounded-lg shadow p-4 my-4 w-full flex flex-col justify-center justify-between cursor-pointer hover:shadow-lg transition-shadow">
-            <h2 className="text-black font-semibold text-lg">{course.name}</h2>
-        </div>
-    );
+  return (
+    <div className="h-20 bg-white rounded-lg shadow p-4 my-4 w-full flex flex-col justify-center justify-between cursor-pointer hover:shadow-lg transition-shadow">
+      <h2 className="text-black font-semibold text-lg">{course.name}</h2>
+    </div>
+  );
 }
--- frontend/src/features/courses/pages/CoursePage.jsx
+++ frontend/src/features/courses/pages/CoursePage.jsx
@@ -4,52 +4,51 @@ import CourseCard from "../components/CourseCard";
 import NavBar from "../components/NavBar";
 
 export default function CoursePage() {
-    const [courses, setCourses] = useState([]);
-    const [loading, setLoading] = useState(true);
+  const [courses, setCourses] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() =>{
-        const fetchCourses = async () => {
-            try{
-                const response = await fetch("http://localhost:3000/api/v1/courses");
-                const data = await response.json();
-                console.log(data);
-                setCourses(data);
-            }catch(error){
-                //mostrar mensaje de error de alguna forma
-                console.error("Error fetching courses:", error);
-            }finally{
-                setLoading(false);
-            }
-        };
-        fetchCourses();
-    }, []);
-    
-    if(loading){
-        return <div>Cargando materias...</div>;
-    }
+  useEffect(() =>{
+    const fetchCourses = async () => {
+        try{
+          const response = await fetch(`${import.meta.env.VITE_API_URL}/api/v1/courses`);
+          const data = await response.json();
+          console.log(data);
+          setCourses(data);
+        }catch(error){
+          //mostrar mensaje de error de alguna forma
+          console.error("Error fetching courses:", error);
+        }finally{
+          setLoading(false);
+        }
+    };
+    fetchCourses();
+  }, []);
 
-    return (
-        <>
-          <NavBar />
-          <div className="flex justify-center min-h-screen bg-[#f3f8f9] p-4">
-            <div className="flex w-full container gap-4">
-              <div className= "w-1/4 h-4/5 bg-white rounded-lg shadow-md">
-              </div>
-              <div className="justify-start w-3/4 px-6 container">
-                <h1 className="flex text-2xl font-bold p-2 mb-4 text-black">Materias Disponibles</h1>
-                {courses.length === 0 ? (
-                  <div>No hay materias disponibles</div>
-                ) : (
-                    courses.map((course) => (
-                      <CourseCard key={course.id} course={course} />
-                    ))
-                  )}
-              </div>
-              <div className="w-1/4 h-4/5 bg-white rounded-lg shadow-md">
-              </div>
-            </div>
+  if(loading){
+    return <div>Cargando materias...</div>;
+  }
+
+  return (
+   <>
+      <NavBar />
+      <div className="flex justify-center min-h-screen bg-[#f3f8f9] p-4">
+        <div className="flex w-full container gap-4">
+          <div className= "w-1/4 h-4/5 bg-white rounded-lg shadow-md">
           </div>
-        </>
-    );
-
+          <div className="justify-start w-3/4 px-6 container">
+            <h1 className="flex text-2xl font-bold p-2 mb-4 text-black">Materias Disponibles</h1>
+            {courses.length === 0 ? (
+              <div>No hay materias disponibles</div>
+            ) : (
+                courses.map((course) => (
+                  <CourseCard key={course.id} course={course} />
+                ))
+              )}
+          </div>
+          <div className="w-1/4 h-4/5 bg-white rounded-lg shadow-md">
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
