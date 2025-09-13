dfca20700f2100b0d23ef578a770573850b865d4
federico.godin <federico.godin@fing.edu.uy>
2025-09-13 20:55:08 -0300

    ruta visitante

--- frontend/src/app/routes/index.jsx
+++ frontend/src/app/routes/index.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import CoursePage from "@/features/courses/pages/CoursePage";
+import VisitorFlow from "@/features/visitors/pages/VisitorFlow";
 
 export function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/courses" replace />} />
       <Route path="/courses" element={<CoursePage />} />
+      <Route path="/visitor-flow" element={<VisitorFlow />} />
     </Routes>
   );
 }
