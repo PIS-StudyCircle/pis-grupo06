dc5fa0af3f0afffacbfbfb91ae6f1a29f7eca0d4
federico.godin <federico.godin@fing.edu.uy>
2025-09-20 20:34:04 -0300

    no actualizar la contraseña si es igual a la actual

--- backend/app/controllers/api/v1/users/passwords_controller.rb
+++ backend/app/controllers/api/v1/users/passwords_controller.rb
@@ -17,21 +17,33 @@ module Api
 
         # -> restablece la contraseña con el token
         def update
+          token = resource_params[:reset_password_token]
+          password = resource_params[:password]
+
+          user = resource_class.with_reset_password_token(token)
+          if user.nil?
+            return render json: { errors: ["El enlace de restablecimiento es inválido o ya ha sido utilizado."] }, status: :unprocessable_entity
+          end
+
+          # Verificar si la nueva contraseña es igual a la actual.
+          if user.valid_password?(password)
+            user.errors.add(:password, :same_as_current, message: "no puede ser igual a la contraseña actual")
+            return render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+          end
+
           self.resource = resource_class.reset_password_by_token(resource_params)
+
           if resource.errors.empty?
             sign_in(resource_name, resource)
             token, _payload = Warden::JWTAuth::UserEncoder.new.call(resource, :user, nil)
             render json: {
-            message: "Contraseña actualizada correctamente.",
-            user: UserSerializer.new(resource), 
-            token: token
-          }, status: :ok
+              message: "Contraseña actualizada correctamente.",
+              user: UserSerializer.new(resource),
+              token: token
+            }, status: :ok
           else
-            if resource.errors.key?(:reset_password_token)
-              render json: { errors: ["El enlace de restablecimiento es inválido o ya ha sido utilizado."] }, status: :unprocessable_entity
-            else
-              render json: { errors: resource.errors.full_messages }, status: :unprocessable_entity
-            end
+            # Esto captura otros errores de Devise (ej: contraseña muy corta).
+            render json: { errors: resource.errors.full_messages }, status: :unprocessable_entity
           end
         end
 
