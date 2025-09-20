b3e82d80e22adb48b1f5036df72bcfe2e15e6618
rafaella-martino <rafaella.martino@cedarcode.com>
2025-09-20 20:33:12 -0300

    db: agregar seeds de prueba

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -18,4 +18,16 @@ json_data.each do |entry|
   )
 end
 
+test_user = User.find_or_create_by!(email: "test@gmail.com") do |u|
+  u.name                  = "Usuario"
+  u.last_name             = "Prueba"
+  u.password              = "password123"
+  u.password_confirmation = "password123"
+  u.description           = "Test user"
+  u.faculty               = fing
+end
+
+tema_1 = Subject.find_or_create_by!(name: "Tema 1 - Test", course_id: 1, creator_id: test_user.id)
+tema_2 = Subject.find_or_create_by!(name: "Tema 2 - Test", course_id: 1, creator_id: test_user.id)
+
 Rails.logger.debug { "Finished seeding courses. Total courses: #{Course.count}" }
