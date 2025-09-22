cbd573f60113407080c2657b923f418223fc35cc
Camila <cami.cam2906@gmail.com>
2025-09-22 17:51:10 -0300

    Se agrega due date a los subjects y se agregan mas al seed

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -48,84 +48,227 @@ end
 # ------ TEMAS DE MATERIAS ------
 
 # Crear subjects para un curso
+# ========================
 course = Course.find_by(id: 447) # PIS
 creator = User.find_by(email: "anaperez@gmail.com") || User.first
+# ========================
 
 Subject.find_or_create_by!(name: "Análisis de Requerimientos", course: course) do |s|
   s.creator = creator
+  s.due_date = 3.months.from_now
 end
 
 Subject.find_or_create_by!(name: "Gestión de Riesgos", course: course) do |s|
   s.creator = creator
+  s.due_date = 4.months.from_now
 end
 
 Subject.find_or_create_by!(name: "Casos de Uso", course: course) do |s|
   s.creator = creator
+  s.due_date = 5.months.from_now
 end
 
+# ========================
 course = Course.find_by(id: 39) # Cálculo Diferencial e Integral en una variable
 creator = User.find_by(email: "martadaluz@gmail.com") || User.first
+# ========================
 
 Subject.find_or_create_by!(name: "Conjuntos", course: course) do |s|
   s.creator = creator
-end
-Subject.find_or_create_by!(name: "Derivadas", course: course) do |s|
-  s.creator = creator
-end
-Subject.find_or_create_by!(name: "Integrales simples", course: course) do |s|
-  s.creator = creator
-end
-Subject.find_or_create_by!(name: "Axiomas", course: course) do |s|
-  s.creator = creator
+  s.due_date = 6.months.from_now
 end
 
+Subject.find_or_create_by!(name: "Derivadas", course: course) do |s|
+  s.creator = creator
+  s.due_date = 3.months.from_now
+end
+
+Subject.find_or_create_by!(name: "Integrales simples", course: course) do |s|
+  s.creator = creator
+  s.due_date = 4.months.from_now
+end
+
+Subject.find_or_create_by!(name: "Axiomas", course: course) do |s|
+  s.creator = creator
+  s.due_date = 5.months.from_now
+end
+
+# ========================
 course = Course.find_by(id: 116) # Electrotécnica I
 creator = User.find_by(email: "martinramirez@gmail.com") || User.first
+# ========================
 
 Subject.find_or_create_by!(name: "Circuitos Eléctricos Básicos", course: course) do |s|
   s.creator = creator
-end
-Subject.find_or_create_by!(name: "Ley de Ohm y Leyes de Kirchhoff", course: course) do |s|
-  s.creator = creator
-end
-Subject.find_or_create_by!(name: "Resistencias y Asociación de Resistencias", course: course) do |s|
-  s.creator = creator
+  s.due_date = 6.months.from_now
 end
 
-course = Course.find_by(id: 145) # Fisica I
+Subject.find_or_create_by!(name: "Ley de Ohm y Leyes de Kirchhoff", course: course) do |s|
+  s.creator = creator
+  s.due_date = 3.months.from_now
+end
+
+Subject.find_or_create_by!(name: "Resistencias y Asociación de Resistencias", course: course) do |s|
+  s.creator = creator
+  s.due_date = 4.months.from_now
+end
+
+# ========================
+course = Course.find_by(id: 145) # Física I
 creator = User.find_by(email: "sofiagarcia@gmail.com") || User.first
+# ========================
 
 Subject.find_or_create_by!(name: "MCU", course: course) do |s|
   s.creator = creator
-end
-Subject.find_or_create_by!(name: "MRU", course: course) do |s|
-  s.creator = creator
-end
-Subject.find_or_create_by!(name: "Peso y masa", course: course) do |s|
-  s.creator = creator
+  s.due_date = 5.months.from_now
 end
 
+Subject.find_or_create_by!(name: "MRU", course: course) do |s|
+  s.creator = creator
+  s.due_date = 6.months.from_now
+end
+
+Subject.find_or_create_by!(name: "Peso y masa", course: course) do |s|
+  s.creator = creator
+  s.due_date = 3.months.from_now
+end
+
+# ========================
 course = Course.find_by(id: 185) # Geometría y Álgebra Lineal 1
 creator = User.find_by(email: "luciafernandez@gmail.com") || User.first
+# ========================
 
 Subject.find_or_create_by!(name: "Geometría y Álgebra Lineal 1", course: course) do |s|
   s.creator = creator
+  s.due_date = 4.months.from_now
 end
+
 Subject.find_or_create_by!(name: "Operaciones con vectores", course: course) do |s|
   s.creator = creator
+  s.due_date = 5.months.from_now
 end
+
 Subject.find_or_create_by!(name: "Matrices y determinantes", course: course) do |s|
   s.creator = creator
+  s.due_date = 6.months.from_now
 end
+
 Subject.find_or_create_by!(name: "Base y dimensión", course: course) do |s|
   s.creator = creator
+  s.due_date = 3.months.from_now
 end
+
 Subject.find_or_create_by!(name: "Sistemas de ecuaciones lineales", course: course) do |s|
   s.creator = creator
+  s.due_date = 4.months.from_now
 end
+
 Subject.find_or_create_by!(name: "Método de Gauss y Gauss-Jordan", course: course) do |s|
   s.creator = creator
+  s.due_date = 5.months.from_now
 end
+
 Subject.find_or_create_by!(name: "Autovalores y autovectores", course: course) do |s|
   s.creator = creator
+  s.due_date = 6.months.from_now
+end
+
+# ========================
+course = Course.find_by(id: 3) #Administración de Infraestructuras
+creator = User.find_by(email: "anaperez@gmail.com") || User.first
+# ========================
+
+Subject.find_or_create_by!(name: "Gestión de Servidores", course: course) do |s|
+  s.creator = creator
+  s.due_date = 3.months.from_now
+end
+
+Subject.find_or_create_by!(name: "Redes y Comunicaciones", course: course) do |s|
+  s.creator = creator
+  s.due_date = 4.months.from_now
+end
+
+Subject.find_or_create_by!(name: "Seguridad en Infraestructura", course: course) do |s|
+  s.creator = creator
+  s.due_date = 5.months.from_now
+end
+
+Subject.find_or_create_by!(name: "Monitoreo y Mantenimiento", course: course) do |s|
+  s.creator = creator
+  s.due_date = 6.months.from_now
+end
+
+# ========================
+course = Course.find_by(id: 5) #Administración de Operaciones
+creator = User.find_by(email: "luisgomez@gmail.com") || User.first
+# ========================
+
+Subject.find_or_create_by!(name: "Gestión de Procesos", course: course) do |s|
+  s.creator = creator
+  s.due_date = 3.months.from_now
+end
+
+Subject.find_or_create_by!(name: "Planificación de la Producción", course: course) do |s|
+  s.creator = creator
+  s.due_date = 4.months.from_now
+end
+
+Subject.find_or_create_by!(name: "Gestión de la Calidad", course: course) do |s|
+  s.creator = creator
+  s.due_date = 5.months.from_now
+end
+
+Subject.find_or_create_by!(name: "Logística y Cadena de Suministro", course: course) do |s|
+  s.creator = creator
+  s.due_date = 6.months.from_now
+end
+
+# ========================
+course = Course.find_by(id: 8) #Agrimensura Legal 1
+creator = User.find_by(email: "luisgomez@gmail.com") || User.first
+# ========================
+
+Subject.find_or_create_by!(name: "Levantamiento Topográfico", course: course) do |s|
+  s.creator = creator
+  s.due_date = 3.months.from_now
+end
+
+Subject.find_or_create_by!(name: "Catastro y Planimetría", course: course) do |s|
+  s.creator = creator
+  s.due_date = 4.months.from_now
+end
+
+Subject.find_or_create_by!(name: "Límites y Propiedad", course: course) do |s|
+  s.creator = creator
+  s.due_date = 5.months.from_now
+end
+
+Subject.find_or_create_by!(name: "Documentación Legal y Escrituras", course: course) do |s|
+  s.creator = creator
+  s.due_date = 6.months.from_now
+end
+
+# ========================
+course = Course.find_by(id: 13) #Algoritmos Evolutivos
+creator = User.find_by(email: "mariarodriguez@gmail.com") || User.first
+# ========================
+
+Subject.find_or_create_by!(name: "Introducción a Algoritmos Evolutivos", course: course) do |s|
+  s.creator = creator
+  s.due_date = 3.months.from_now
+end
+
+Subject.find_or_create_by!(name: "Algoritmos Genéticos", course: course) do |s|
+  s.creator = creator
+  s.due_date = 4.months.from_now
+end
+
+Subject.find_or_create_by!(name: "Algoritmos de Enjambre", course: course) do |s|
+  s.creator = creator
+  s.due_date = 5.months.from_now
+end
+
+Subject.find_or_create_by!(name: "Optimización Multiobjetivo", course: course) do |s|
+  s.creator = creator
+  s.due_date = 6.months.from_now
 end
