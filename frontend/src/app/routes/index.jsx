650ef57162b2816485e62ad9641b7d10d98896bf
Agustin <agustin@DESKTOP-D6Q4Q3F.localdomain>
2025-09-25 16:19:46 -0300

    Emprolijado rutas

--- frontend/src/app/routes/index.jsx
+++ frontend/src/app/routes/index.jsx
@@ -1,11 +1,10 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import PrivateRoute from "./PrivateRoute";
-import { SignInPage, RegisterPage, TutorPage, VisitorFlow } from "@/features/users";
-import { RequireGuestRoute } from "./RequireGuestRoute";
-import ProfilePage from "@/features/users/pages/Profile";
-
-
 import { CoursePage, CourseDetailPage } from "@/features/courses";
+import { ProfilePage, SignInPage, RegisterPage, TutorPage, VisitorFlow } from "@/features/users";
+import { RequireGuestRoute } from "./RequireGuestRoute";
+
+
 
 
 export function AppRoutes() {
@@ -14,17 +13,17 @@ export function AppRoutes() {
       <Route element={<RequireGuestRoute />}>
         <Route path="/iniciar_sesion" element={<SignInPage />} />
         <Route path="/registrarse" element={<RegisterPage />} />
-        <Route path="/tutores" element={<TutorPage />} />
         <Route path="/flujo-visitante" element={<VisitorFlow />} />
       </Route>
-
-      <Route path="/" element={<Navigate to="/materias" replace />} />
 
       <Route path="/materias" element={<CoursePage />} />
       <Route element={<PrivateRoute />}>
         <Route path="/materias/:courseId" element={<CourseDetailPage />} />
         <Route path="/perfil" element={<ProfilePage />} />
       </Route>
+
+      <Route path="/" element={<Navigate to="/materias" replace />} />
+      <Route path="/tutores" element={<TutorPage />} />
     </Routes>
   );
 }
--- frontend/src/features/users/index.jsx
+++ frontend/src/features/users/index.jsx
@@ -2,3 +2,4 @@ export { default as SignInPage } from "./pages/SignInPage";
 export { default as RegisterPage } from "./pages/RegisterPage";
 export { default as TutorPage } from "./pages/TutorPage";
 export { default as VisitorFlow } from "./pages/VisitorFlow";
+export { default as ProfilePage } from "./pages/Profile";
