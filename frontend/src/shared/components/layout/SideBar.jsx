c70866e80bdbba8638a9a2ce4bebb0f8a82da8a1
Agustin <agustin@DESKTOP-D6Q4Q3F.localdomain>
2025-09-25 16:05:32 -0300

    Agregado imagen cruz

--- frontend/src/shared/components/layout/SideBar.jsx
+++ frontend/src/shared/components/layout/SideBar.jsx
@@ -99,7 +99,20 @@ const Sidebar = ({
               aria-label="Cerrar sidebar"
               title="Cerrar sidebar"
             >
-              ✕
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="size-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M6 18 18 6M6 6l12 12"
+                />
+              </svg>
             </button>
           )}
         </div>
@@ -161,7 +174,20 @@ const Sidebar = ({
               aria-label="Cerrar"
               title="Cerrar"
             >
-              ✕
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="size-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M6 18 18 6M6 6l12 12"
+                />
+              </svg>
             </button>
           </div>
 
