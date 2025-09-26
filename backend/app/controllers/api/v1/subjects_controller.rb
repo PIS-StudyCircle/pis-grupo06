cf9cb4bda05ca8cf372e89d7e19e7af235c0a5d6
Maria Paz <mapi.osorio@gmail.com>
2025-09-26 14:54:55 -0300

    backend listar tutorias de tema

--- backend/app/controllers/api/v1/subjects_controller.rb
+++ backend/app/controllers/api/v1/subjects_controller.rb
@@ -2,6 +2,7 @@ module Api
   module V1
     class SubjectsController < ApplicationController
       include Pagy::Backend
+      include JsonResponse
 
       before_action :authenticate_user!, only: [:create]
 
@@ -21,6 +22,38 @@ module Api
           subjects: @subjects,
           pagination: pagy_metadata(@pagy)
         }
+      end
+
+      def show
+        subject = Course.find(params[:course_id]).subjects.find(params[:id])
+
+        # Ver a que se refieren con "active tutorings"
+        active_tutorings = subject.tutorings
+                                  .upcoming
+                                  .order(:scheduled_at)
+
+        data = {
+          id: subject.id,
+          name: subject.name,
+          course_id: subject.course_id,
+          tutorings: active_tutorings.map do |t|
+            {
+              id: t.id,
+              scheduled_at: t.scheduled_at,
+              duration_mins: t.duration_mins,
+              modality: t.modality,
+              capacity: t.capacity,
+              enrolled: t.enrolled,
+              seats_left: t.capacity - t.enrolled,
+              tutor_id: t.tutor_id
+            }
+          end
+        }
+
+        success_response(message: "Tema y tutorias cargadas con exito", data: data)
+        
+      rescue ActiveRecord::RecordNotFound
+        error_response(message: "Tema no encontrado", status: :not_found)
       end
 
       def create
