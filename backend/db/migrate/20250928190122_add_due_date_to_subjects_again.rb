b76fa31a3335bb41cc3bf5be3050f5d8ea9cb26f
Agustin Castro <agustincastro2003@gmail.com>
2025-09-28 17:27:41 -0300

    Eliminadno migrations mal

--- backend/db/migrate/20250928190122_add_due_date_to_subjects_again.rb
+++
@@ -1,5 +0,0 @@
-class AddDueDateToSubjectsAgain < ActiveRecord::Migration[8.0]
-  def change
-    add_column :subjects, :due_date, :date
-  end
-end
--- backend/db/migrate/20250928193245_add_enrolled_to_tutorings.rb
+++
@@ -1,7 +0,0 @@
-class AddEnrolledToTutorings < ActiveRecord::Migration[8.0]
-  def change
-    unless column_exists?(:tutorings, :enrolled)
-      add_column :tutorings, :enrolled, :integer, default: 0, null: false
-    end
-  end
-end
