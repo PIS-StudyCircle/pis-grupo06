6c4769950534880dc9e3e41edd21ea5c62f11f78
Agustin Castro <agustincastro2003@gmail.com>
2025-10-09 15:58:34 -0300

    Eliminar useEffect

--- frontend/src/features/users/components/ProfilePhotoEditor.jsx
+++ frontend/src/features/users/components/ProfilePhotoEditor.jsx
@@ -1,4 +1,4 @@
-import { useState, useCallback, useEffect } from "react";
+import { useState, useCallback } from "react";
 import Cropper from "react-easy-crop";
 import { getCroppedImg } from "@utils/cropImage";
 import { RotateCwSquare } from "lucide-react";
