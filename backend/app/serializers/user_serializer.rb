1ebd2522a0a8826fd58bd8cb6de9ff57c676b60c
Agustin Castro <agustincastro2003@gmail.com>
2025-10-09 15:59:48 -0300

    Arregelo gemfile

--- backend/app/serializers/user_serializer.rb
+++ backend/app/serializers/user_serializer.rb
@@ -1,10 +1,10 @@
 class UserSerializer
   include JSONAPI::Serializer
-  include Rails.application.routes.url_helpers 
+  include Rails.application.routes.url_helpers
 
   attributes :id, :email, :name, :last_name, :description, :created_at, :updated_at
 
-  attribute :profile_photo_url  do |user|
+  attribute :profile_photo_url do |user|
     if user.profile_photo.attached?
       Rails.application.routes.url_helpers.url_for(user.profile_photo)
     else
