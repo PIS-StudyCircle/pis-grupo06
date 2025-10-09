f5bcc1cc58b38804fc3f37b1555485778ccac6b6
Agustin Castro <agustincastro2003@gmail.com>
2025-10-09 15:32:18 -0300

    Pronto

--- frontend/src/features/users/components/AuthLayout.jsx
+++ frontend/src/features/users/components/AuthLayout.jsx
@@ -1,11 +1,17 @@
 import { Link } from "react-router-dom";
 import logo from "@/assets/logo_completo.png";
 
-export function AuthLayout({ title, children, footerText, footerLink, footerLinkText, showLogo = true }) {
+export function AuthLayout({
+  title,
+  children,
+  footerText,
+  footerLink,
+  footerLinkText,
+  showLogo = true,
+}) {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="w-full max-w-md p-8 space-y-6 bg-white rounded-xl shadow-lg">
-        
         {/* Encabezado */}
         <div className="text-center">
           {showLogo && (
@@ -17,12 +23,10 @@ export function AuthLayout({ title, children, footerText, footerLink, footerLink
               />
             </Link>
           )}
-          <h1 className="text-2xl font-bold text-gray-800 mt-4">
-            {title}
-          </h1>
+          <h1 className="text-2xl font-bold text-gray-800 mt-4">{title}</h1>
         </div>
 
-        {children}}
+        {children}
         <p className="text-sm text-center text-gray-600">
           {footerText}{" "}
           <a
