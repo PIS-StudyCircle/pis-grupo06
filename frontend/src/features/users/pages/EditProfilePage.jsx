fd40a5b6a49aabc5b03749ebbd5f4e6840ae3e8b
rafaella-martino <rafaella.martino@cedarcode.com>
2025-11-05 18:54:02 -0300

    fix

--- frontend/src/features/users/pages/EditProfilePage.jsx
+++ frontend/src/features/users/pages/EditProfilePage.jsx
@@ -55,7 +55,7 @@ export default function EditProfilePage() {
       setField("profile_photo", null);
       setPreview(user.profile_photo_url || DEFAULT_PHOTO);
     }
-  }, [user, sertField]);
+  }, [user, setField]);
 
   const handleFileChange = (e) => {
     const file = e.target.files?.[0];
