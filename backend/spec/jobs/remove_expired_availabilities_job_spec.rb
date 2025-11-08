d1d889602f27024ff00b873956b6b9f6b6dc853f
rafaella-martino <rafaella.martino@cedarcode.com>
2025-11-08 12:30:22 -0300

    rubocop fix

--- backend/spec/jobs/remove_expired_availabilities_job_spec.rb
+++ backend/spec/jobs/remove_expired_availabilities_job_spec.rb
@@ -142,7 +142,7 @@ RSpec.describe RemoveExpiredAvailabilitiesJob, type: :job do
         )
       end
 
-      let!(:expired_available_1) do
+      let!(:expired_available1) do
         TutoringAvailability.create!(
           tutoring: tutoring,
           start_time: current_time - 3.hours,
@@ -160,7 +160,7 @@ RSpec.describe RemoveExpiredAvailabilitiesJob, type: :job do
         )
       end
 
-      let!(:expired_available_2) do
+      let!(:expired_available2) do
         TutoringAvailability.create!(
           tutoring: tutoring,
           start_time: current_time - 1.hour,
@@ -185,8 +185,8 @@ RSpec.describe RemoveExpiredAvailabilitiesJob, type: :job do
 
         expect(TutoringAvailability.exists?(expired_booked.id)).to be true
         expect(TutoringAvailability.exists?(future_available.id)).to be true
-        expect(TutoringAvailability.exists?(expired_available_1.id)).to be false
-        expect(TutoringAvailability.exists?(expired_available_2.id)).to be false
+        expect(TutoringAvailability.exists?(expired_available1.id)).to be false
+        expect(TutoringAvailability.exists?(expired_available2.id)).to be false
       end
     end
 
