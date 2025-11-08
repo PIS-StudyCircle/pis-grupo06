590dfa9d70fd15f07196f7093136a5fcb32092f1
KevinMacTec <kevinmactec@gmail.com>
2025-11-08 16:47:01 -0300

    Agregue una columna que no quería y la borre, ahí actualice el scheema

--- backend/db/migrate/20251108183314_add_last_notified_message_id_to_chat_users.rb
+++ backend/db/migrate/20251108183314_add_last_notified_message_id_to_chat_users.rb
@@ -1,13 +1,9 @@
 class AddLastNotifiedMessageIdToChatUsers < ActiveRecord::Migration[8.0]
   def change
-    unless column_exists?(:chat_users, :last_notified_message_id)
-      add_column :chat_users, :last_notified_message_id, :bigint
-      add_index  :chat_users, :last_notified_message_id
-    end
+    add_column :chat_users, :last_notified_message_id, :bigint
+    add_index  :chat_users, :last_notified_message_id
 
-    if column_exists?(:chat_users, :last_notified_at)
-      remove_index :chat_users, :last_notified_at if index_exists?(:chat_users, :last_notified_at)
-      remove_column :chat_users, :last_notified_at, :datetime
-    end
+    add_column :chat_users, :last_read_at, :datetime
+    add_index  :chat_users, :last_read_at
   end
 end
