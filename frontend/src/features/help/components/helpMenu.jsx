fce5c169c077ff6d86c47e53344426104c7041af
Dumas <dumas.bentancur.silvera@fing.edu.uy>
2025-11-11 21:31:50 -0300

    Comentarios de ayuda sobre chat y editar perfil

--- frontend/src/features/help/components/helpMenu.jsx
+++ frontend/src/features/help/components/helpMenu.jsx
@@ -15,114 +15,127 @@ export default function HelpMenu() {
         const pure = path.split("?")[0].split("#")[0] || "/";
         // /materias
         if (pure === "/" || pure === "/materias") {
-        return [
-            "Bienvenido a Study Circle.",
-            "En esta página podés explorar todas las materias disponibles, buscando por nombre.",
-            "Podés guardar tus favoritas desde la estrella.",
-            "Clickeando en una materia podés ver los temas de esta, solicitar tutoría o brindar una.",
-            "Usá el menú para navegar entre Materias, Tutorías, Mis Clases y Usuarios:",
-            "En Tutorías: consultá las tutorías disponibles y solicitadas.",
-            "En Mis clases: organiza tus próximas tutorías y las que ya tuviste.",
-            "En Usuarios: buscá otros usuarios y mirá sus reseñas.",
-            "Desde el icono de perfil podés acceder a tu información personal, editarla o cerrar sesión.",
-        ];
+            return [
+                "Bienvenido a Study Circle.",
+                "En esta página podés explorar todas las materias disponibles, buscando por nombre.",
+                "Podés guardar tus favoritas desde la estrella.",
+                "Clickeando en una materia podés ver los temas de esta, solicitar tutoría o brindar una.",
+                "Usá el menú para navegar entre Materias, Tutorías, Mis Clases y Usuarios.",
+                "En Tutorías: consultá las tutorías disponibles y solicitadas.",
+                "En Mis clases: organiza tus próximas tutorías y las que ya tuviste.",
+                "En Usuarios: buscá otros usuarios y mirá sus reseñas.",
+                "Desde el icono de perfil podés acceder a tu información personal, editarla o cerrar sesión.",
+            ];
         }
         // /materias/:id
         if (/^\/materias\/\d+$/i.test(pure)) {
-        return [
-            "En esta página ves la información general de la materia y los temas asociados.",
-            "Podés elegir entre “Recibir tutoría” para pedir una tutoría como estudiante o “Brindar tutoría” para ofrecerte como tutor. Al hacerlo podés crear nuevos temas si no está el que querés.",
-            "También podés consultar las tutorías disponibles sobre un tema clickeando en este.",
-        ];
+            return [
+                "En esta página ves la información general de la materia y los temas asociados.",
+                "Podés elegir entre “Recibir tutoría” para pedir una tutoría como estudiante o “Brindar tutoría” para ofrecerte como tutor. Al hacerlo podés crear nuevos temas si no está el que querés.",
+                "También podés consultar las tutorías disponibles sobre un tema clickeando en este.",
+            ];
         }
         // /materias/:id/temas/:id
         if (/^\/materias\/\d+\/temas\/\d+$/i.test(pure)) {
-        return [
-            "Aquí podés ver las tutorías disponibles y solicitadas para este tema.",
-            "Podés elegir entre \"Recibir tutoría\" para pedir una tutoría como estudiante, \"Brindar tutoría\" para ofrecerte como tutor o \"Desuscribirme\" si ya no querés participar.",
-        ];
+            return [
+                "Aquí podés ver las tutorías disponibles y solicitadas para este tema.",
+                "Podés elegir entre \"Recibir tutoría\" para pedir una tutoría como estudiante, \"Brindar tutoría\" para ofrecerte como tutor o \"Desuscribirme\" si ya no querés participar.",
+            ];
         }
         // /tutorias
         if (pure === "/tutorias") {
-        return [
-            "Aquí podés encontrar todas las tutorías brindadas y solicitadas. Podés buscar por materia o tema, y filtrar por aquellas que no tienen tutor asignado.",
-            "Podés unirte como tutor apretando en “ser tutor” o como estudiante apretando en “unirme”.",
-        ];
+            return [
+                "Aquí podés encontrar todas las tutorías brindadas y solicitadas. Podés buscar por materia o tema, y filtrar por aquellas que no tienen tutor asignado.",
+                "Podés unirte como tutor apretando en “ser tutor” o como estudiante apretando en “unirme”.",
+            ];
+        }
+        // /tutorias/:id
+        if (/^\/tutorias\/\d+$/i.test(pure)) {
+            return [
+                "En esta página podés ver toda la información de la tutoría.",
+                "Una vez haya un tutor y al menos un estudiante, tendrán disponible un chat para coordinar los detalles. Este chat ya no será accesible una vez finalizada la tutoría.",
+            ];
         }
         // /tutorias/:id/elegir_horario_estudiante
         if (/^\/tutorias\/\d+\/elegir_horario_estudiante$/i.test(pure)) {
-        return [
-            "Estos son los horarios que el tutor tiene disponibles, elegí el rango en el que puedas.",
-            "Una vez confirmes, se notificará automáticamente a la otra persona y se agendará la tutoría.",
-        ];
+            return [
+                "Estos son los horarios que el tutor tiene disponibles, elegí el rango en el que puedas.",
+                "Una vez confirmes, se notificará automáticamente a la otra persona y se agendará la tutoría.",
+            ];
         }
         // /tutorias/:id/elegir_horario_tutor
         if (/^\/tutorias\/\d+\/elegir_horario_tutor$/i.test(pure)) {
-        return [
-            "Podés elegir la cantidad de estudiantes que pueden unirse a la tutoría.",
-            "Estos son los horarios que el estudiante tiene disponibles, elegí el rango en el que puedas.",
-            "Una vez confirmes, se notificará automáticamente a la otra persona y se agendará la tutoría.",
-        ];
+            return [
+                "Podés elegir la cantidad de estudiantes que pueden unirse a la tutoría.",
+                "Estos son los horarios que el estudiante tiene disponibles, elegí el rango en el que puedas.",
+                "Una vez confirmes, se notificará automáticamente a la otra persona y se agendará la tutoría.",
+            ];
         }
         // /tutorias/materia/:id
         if (/^\/tutorias\/materia\/\d+$/i.test(pure)) {
-        return [
-            "Aquí podés encontrar todas las tutorías actualmente disponibles para esta materia.",
-            "Podés buscar por tema o solicitar una nueva si no encontrás la que querés.",
-        ];
+            return [
+                "Aquí podés encontrar todas las tutorías actualmente disponibles para esta materia.",
+                "Podés buscar por tema o solicitar una nueva si no encontrás la que querés.",
+            ];
         }
         // /tutorias/ser_tutor/:id
         if (/^\/tutorias\/ser_tutor\/\d+$/i.test(pure)) {
-        return [
-            "Aquí podés encontrar todas las tutorías actualmente solicitadas para esta materia.",
-            "Podés buscar por tema o crear una nueva si no encontrás la que querés.",
-        ];
+            return [
+                "Aquí podés encontrar todas las tutorías actualmente solicitadas para esta materia.",
+                "Podés buscar por tema o crear una nueva si no encontrás la que querés.",
+            ];
         }
         // /tutorias/elegir_temas/estudiante|tutor/:id
         if (/^\/tutorias\/elegir_temas\/(estudiante|tutor)\/\d+$/i.test(pure)) {
-        return [
-            "Seleccioná los temas que quieras para la tutoría o creá nuevos si no los hay.",
-        ];
+            return [
+                "Seleccioná los temas que quieras para la tutoría o creá nuevos si no los hay.",
+            ];
         }
         // /tutorias/crear/:id
         if (/^\/tutorias\/crear\/\d+$/i.test(pure)) {
-        return [
-            "Seleccioná la modalidad, los cupos y tus horarios disponibles para la tutoría.",
-            "Quienes quieran participar elegirán entre estos horarios.",
-        ];
+            return [
+                "Seleccioná la modalidad, los cupos y tus horarios disponibles para la tutoría.",
+                "Quienes quieran participar elegirán entre estos horarios.",
+            ];
         }
         // /tutorias/solicitar/:id
         if (/^\/tutorias\/solicitar\/\d+$/i.test(pure)) {
-        return [
-            "Seleccioná la modalidad, tus horarios disponibles para la tutoría y dejá un comentario (como tu preferencia de los cupos).",
-            "Quien acepte brindar la tutoría elegirá entre estos horarios y definirá el cupo.",
-        ];
+            return [
+                "Seleccioná la modalidad, tus horarios disponibles para la tutoría y dejá un comentario (como tu preferencia de los cupos).",
+                "Quien acepte brindar la tutoría elegirá entre estos horarios y definirá el cupo.",
+            ];
         }
         // /notificaciones
         if (pure === "/notificaciones") {
-        return [
-            "En esta pantalla podés ver tus próximas tutorías, pudiendo desuscribirte de una si ya no podés asistir.",
-            "También podés ver las tutorías en las que ya participaste y dejar una clasificación al tutor.",
-        ];
+            return [
+                "En esta pantalla podés ver tus próximas tutorías, pudiendo desuscribirte de una si ya no podés asistir.",
+                "También podés ver las tutorías en las que ya participaste y dejar una clasificación al tutor.",
+            ];
         }
         // /usuarios
         if (pure === "/usuarios") {
-        return [
-            "Aquí podés ver todos los usuarios, entrá a un perfil para ver las reseñas sobre el usuario.",
-        ];
+            return [
+                "Aquí podés ver todos los usuarios, entrá a un perfil para ver las reseñas sobre el usuario.",
+            ];
         }
         // /perfil
         if (pure === "/perfil") {
-        return [
-            "En tu perfil se muestran tus materias favoritas y las reseñas que recibiste.",
-            "Si es tu perfil, desde 'Editar perfil' podés cambiar tu foto, nombre, apellido o descripción.",
-        ];
+            return [
+                "En tu perfil se muestran tus materias favoritas y las reseñas que recibiste.",
+                "Podés cambiar tu foto, nombre, apellido y descripción desde 'Editar perfil' en el menú.",
+            ];
         }
         // /usuarios/:id
         if (/^\/usuarios\/\d+$/i.test(pure)) {
-        return [
-            "Estás viendo el perfil de otra persona, podés leer las reseñas que recibió y, si compartieron una tutoría, dejarle una nueva.",
-        ];
+            return [
+                "Estás viendo el perfil de otra persona, podés leer las reseñas que recibió y, si compartieron una tutoría, dejarle una nueva.",
+            ];
+        }
+        // /editar-perfil
+        if (pure === "/editar-perfil") {
+            return [
+                "Desde esta página podés editar tu información personal: nombre, apellido, descripción y foto de perfil.",
+            ];
         }
         return [
         "Usá el menú para navegar entre Materias, Tutorías, Mis Clases y Usuarios.",
